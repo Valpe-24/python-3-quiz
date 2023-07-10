75aee62260a92920f9accc4c97cdbc6f28f129f2
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-07-10 10:34:23 -0400

    complete validate_file

--- name_checker.py
+++ name_checker.py
@@ -1,13 +1,26 @@
 from ValidationException import ValidationException
 
 def validate_file(file_path):
-    pass #TODO: Only add code inside this function.
+    numbers = '0123456789'
+    lines = []
+    with open(file_path, "r") as file1:
+        while True:
+            line = file1.readline().split()
 
+            if not line:
+                break
+            lines.append(line)
+
+    for i in range(len(lines)):
+        for letters in lines[i][0]:
+            if letters in numbers:
+                raise ValidationException(f"Invalid first name: {lines[i][0]}")
 
 def test():
     try:
         validate_file("users.txt")
     except ValidationException as ve:
         print(ve)
+
 
 test()
